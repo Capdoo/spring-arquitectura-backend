48acdbe6799cab2ac34aa5ca1ad51f86f695dc56
unknown <mazher_2014@hotmail.com>
2022-06-03 10:05:12 -0500

    ev1-pt2-PuenteTermicoViga2

--- src/main/java/com/arquitectura/app/modules/termico/env1/Env1ServicePart2.java
+++ src/main/java/com/arquitectura/app/modules/termico/env1/Env1ServicePart2.java
@@ -47,14 +47,54 @@ public class Env1ServicePart2 {
 		double[] muroConCamaraAire1 = this.muroConCamaraAire1(excelGetData);
 		double[] muroConCamaraAire2 = this.muroConCamaraAire2(excelGetData);
 
-		
-		double U1 = Math.round(muroSinCamaraAire1[0]/muroSinCamaraAire1[1]*1000.0)/1000.0;
-		double U2 = Math.round(muroSinCamaraAire2[0]/muroSinCamaraAire2[1]*1000.0)/1000.0;
-		double U3 = Math.round(muroConCamaraAire1[0]/muroConCamaraAire1[1]*1000.0)/1000.0;
-		double U4 = Math.round(muroConCamaraAire2[0]/muroConCamaraAire2[1]*1000.0)/1000.0;
+		double[] puenteTermico1 = this.puenteTermico1(excelGetData);
+		double[] puenteTermico2 = this.puenteTermico2(excelGetData);
+		double[] puenteTermicoSobrecimiento1 = this.puenteTermicoSobrecimiento1(excelGetData);
+		double[] puenteTermicoSobrecimiento2 = this.puenteTermicoSobrecimiento2(excelGetData);
 
 		
-		return U1+U2+U3+U4;
+		double[] puenteTermicoViga1 = this.puenteTermicoViga1(excelGetData);
+		double[] puenteTermicoViga2 = this.puenteTermicoViga2(excelGetData);
+
+		
+		//------------------------------------
+
+		double SU1 = muroSinCamaraAire1[0];
+		double SU2 = muroSinCamaraAire2[0];
+		double SU3 = muroConCamaraAire1[0];
+		double SU4 = muroConCamaraAire2[0];
+		
+		double SU5 = puenteTermico1[0];
+		double SU6 = puenteTermico2[0];
+		double SU7 = puenteTermicoSobrecimiento1[0];
+		double SU8 = puenteTermicoSobrecimiento2[0];
+		
+		double SU9 = puenteTermicoViga1[0];
+		double SU10 = puenteTermicoViga2[0];
+
+		
+		
+		double allSxU = SU1 + SU2 + SU3 + SU4 + SU5 + SU6 + SU7 + SU8 + SU9 + SU10;
+		
+		
+		double S1 = muroSinCamaraAire1[1];
+		double S2 = muroSinCamaraAire2[1];
+		double S3 = muroConCamaraAire1[1];
+		double S4 = muroConCamaraAire2[1];
+		
+		double S5 = puenteTermico1[1];
+		double S6 = puenteTermico2[1];
+		double S7 = puenteTermicoSobrecimiento1[1];
+		double S8 = puenteTermicoSobrecimiento2[1];
+
+		double S9 = puenteTermicoViga1[1];
+		double S10 = puenteTermicoViga1[1];
+
+		
+		double allS = S1 + S2 + S3 + S4 + S5 + S6 + S7 + S8 + S9 + S10;
+			
+		
+		return allSxU/allS;
 	}
 	
 	
@@ -127,7 +167,7 @@ public class Env1ServicePart2 {
 	public double formulaUTransmitanciaBloque1(double rse, double rsi, double rst) {
 
 		double U = 1/((this.ESP1/this.COEF1) + (this.ESP2/this.COEF2) + (this.ESP3/this.COEF3) + rse + rsi + rst);
-		U = Math.round(U*100000.0)/100000.0;
+		U = Math.round(U*1000.0)/1000.0;
 
 		return U;
 	}
@@ -202,6 +242,200 @@ public class Env1ServicePart2 {
 		return res;
 		
 	}
+	
+	public double[] puenteTermico1(FormExcelGetData ex) {
+		
+		int i = 77;
+		
+		double[] res = new double[2];
+		
+		this.EL1 = ex.getDataStringColumnAndRow("B",i+"");
+		this.EL2 = ex.getDataStringColumnAndRow("B",(i+1)+"");	
+		this.EL3 = ex.getDataStringColumnAndRow("B",(i+2)+"");	
+
+		this.ESP1 = ex.getDataDecimalFromColumnAndRow("C",i+"");
+		this.ESP2 = ex.getDataDecimalFromColumnAndRow("C",(i+1)+"");
+		this.ESP3 = ex.getDataDecimalFromColumnAndRow("C",(i+2)+"");
+
+		this.AR1 = ex.getDataDecimalFromColumnAndRow("D",i+"");
+
+		this.COEF1 = env1Service.getCoefTransByName(this.EL1);
+		this.COEF2 = env1Service.getCoefTransByName(this.EL2);
+		this.COEF3 = env1Service.getCoefTransByName(this.EL3);
+
+		double U = this.formulaUTransmitanciaBloque1(0.0, 0.0, 0.0);
+		logger.info(U+" : Esta es U5");
+
+		
+		res[0] = this.AR1*U;
+		res[1] = this.AR1;
+				
+		return res;
+	}
+	
+	public double[] puenteTermico2(FormExcelGetData ex) {
+		
+		int i = 85;
+		
+		double[] res = new double[2];
+		
+		this.EL1 = ex.getDataStringColumnAndRow("B",i+"");
+		this.EL2 = ex.getDataStringColumnAndRow("B",(i+1)+"");	
+		this.EL3 = ex.getDataStringColumnAndRow("B",(i+2)+"");	
+
+		this.ESP1 = ex.getDataDecimalFromColumnAndRow("C",i+"");
+		this.ESP2 = ex.getDataDecimalFromColumnAndRow("C",(i+1)+"");
+		this.ESP3 = ex.getDataDecimalFromColumnAndRow("C",(i+2)+"");
+
+		this.AR1 = ex.getDataDecimalFromColumnAndRow("D",i+"");
+
+		this.COEF1 = env1Service.getCoefTransByName(this.EL1);
+		this.COEF2 = env1Service.getCoefTransByName(this.EL2);
+		this.COEF3 = env1Service.getCoefTransByName(this.EL3);
+
+		double U = this.formulaUTransmitanciaBloque1(0.0, 0.0, 0.0);
+		logger.info(U+" : Esta es U6");
+
+		
+		res[0] = this.AR1*U;
+		res[1] = this.AR1;
+				
+		return res;
+	}
+	
+	public double[] puenteTermicoSobrecimiento1(FormExcelGetData ex) {
+		
+		int i = 93;
+		
+		double[] res = new double[2];
+		
+		this.EL1 = ex.getDataStringColumnAndRow("B",i+"");
+		this.EL2 = ex.getDataStringColumnAndRow("B",(i+1)+"");	
+		this.EL3 = ex.getDataStringColumnAndRow("B",(i+2)+"");	
+
+		this.ESP1 = ex.getDataDecimalFromColumnAndRow("C",i+"");
+		this.ESP2 = ex.getDataDecimalFromColumnAndRow("C",(i+1)+"");
+		this.ESP3 = ex.getDataDecimalFromColumnAndRow("C",(i+2)+"");
+
+		this.AR1 = ex.getDataDecimalFromColumnAndRow("D",i+"");
+
+		this.COEF1 = env1Service.getCoefTransByName(this.EL1);
+		this.COEF2 = env1Service.getCoefTransByName(this.EL2);
+		this.COEF3 = env1Service.getCoefTransByName(this.EL3);
+
+		double U = this.formulaUTransmitanciaBloque1(0.0, 0.0, 0.0);
+		
+		logger.info(this.AR1+" : Esta es Area");
+		logger.info(U+" : Esta es U7");
+		logger.info(U*this.AR1+" : Esta es SxU");
+
+		
+		res[0] = this.AR1*U;
+		res[1] = this.AR1;
+				
+		return res;
+	}
+	
+	public double[] puenteTermicoSobrecimiento2(FormExcelGetData ex) {
+		
+		int i = 101;
+		
+		double[] res = new double[2];
+		
+		this.EL1 = ex.getDataStringColumnAndRow("B",i+"");
+		this.EL2 = ex.getDataStringColumnAndRow("B",(i+1)+"");	
+		this.EL3 = ex.getDataStringColumnAndRow("B",(i+2)+"");	
+
+		this.ESP1 = ex.getDataDecimalFromColumnAndRow("C",i+"");
+		this.ESP2 = ex.getDataDecimalFromColumnAndRow("C",(i+1)+"");
+		this.ESP3 = ex.getDataDecimalFromColumnAndRow("C",(i+2)+"");
+
+		this.AR1 = ex.getDataDecimalFromColumnAndRow("D",i+"");
+
+		this.COEF1 = env1Service.getCoefTransByName(this.EL1);
+		this.COEF2 = env1Service.getCoefTransByName(this.EL2);
+		this.COEF3 = env1Service.getCoefTransByName(this.EL3);
+
+		double U = this.formulaUTransmitanciaBloque1(0.0, 0.0, 0.0);
+		
+		logger.info(this.AR1+" : Esta es Area");
+		logger.info(U+" : Esta es U8");
+		logger.info(U*this.AR1+" : Esta es SxU");
+
+		
+		res[0] = this.AR1*U;
+		res[1] = this.AR1;
+				
+		return res;
+	}
+	
+	
+	public double[] puenteTermicoViga1(FormExcelGetData ex) {
+		
+		int i = 109;
+		
+		double[] res = new double[2];
+		
+		this.EL1 = ex.getDataStringColumnAndRow("B",i+"");
+		this.EL2 = ex.getDataStringColumnAndRow("B",(i+1)+"");	
+		this.EL3 = ex.getDataStringColumnAndRow("B",(i+2)+"");	
+
+		this.ESP1 = ex.getDataDecimalFromColumnAndRow("C",i+"");
+		this.ESP2 = ex.getDataDecimalFromColumnAndRow("C",(i+1)+"");
+		this.ESP3 = ex.getDataDecimalFromColumnAndRow("C",(i+2)+"");
+
+		this.AR1 = ex.getDataDecimalFromColumnAndRow("D",i+"");
+
+		this.COEF1 = env1Service.getCoefTransByName(this.EL1);
+		this.COEF2 = env1Service.getCoefTransByName(this.EL2);
+		this.COEF3 = env1Service.getCoefTransByName(this.EL3);
+
+		double U = this.formulaUTransmitanciaBloque1(0.0, 0.0, 0.0);
+		
+		logger.info(this.AR1+" : Esta es Area");
+		logger.info(U+" : Esta es U9");
+		logger.info(U*this.AR1+" : Esta es SxU");
+
+		
+		res[0] = this.AR1*U;
+		res[1] = this.AR1;
+				
+		return res;
+	}
+
+	public double[] puenteTermicoViga2(FormExcelGetData ex) {
+		
+		int i = 117;
+		
+		double[] res = new double[2];
+		
+		this.EL1 = ex.getDataStringColumnAndRow("B",i+"");
+		this.EL2 = ex.getDataStringColumnAndRow("B",(i+1)+"");	
+		this.EL3 = ex.getDataStringColumnAndRow("B",(i+2)+"");	
+
+		this.ESP1 = ex.getDataDecimalFromColumnAndRow("C",i+"");
+		this.ESP2 = ex.getDataDecimalFromColumnAndRow("C",(i+1)+"");
+		this.ESP3 = ex.getDataDecimalFromColumnAndRow("C",(i+2)+"");
+
+		this.AR1 = ex.getDataDecimalFromColumnAndRow("D",i+"");
+
+		this.COEF1 = env1Service.getCoefTransByName(this.EL1);
+		this.COEF2 = env1Service.getCoefTransByName(this.EL2);
+		this.COEF3 = env1Service.getCoefTransByName(this.EL3);
+
+		double U = this.formulaUTransmitanciaBloque1(0.0, 0.0, 0.0);
+		
+		logger.info(this.AR1+" : Esta es Area");
+		logger.info(U+" : Esta es U10");
+		logger.info(U*this.AR1+" : Esta es SxU");
+
+		
+		res[0] = this.AR1*U;
+		res[1] = this.AR1;
+				
+		return res;
+	}
+	
 }
 
 
