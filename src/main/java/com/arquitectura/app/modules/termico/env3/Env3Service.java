95c6c95eeff0dffda5b8663c6495a6e2a164ffe6
unknown <mazher_2014@hotmail.com>
2022-06-10 10:39:59 -0500

    ref repos

--- src/main/java/com/arquitectura/app/modules/termico/env3/Env3Service.java
+++ src/main/java/com/arquitectura/app/modules/termico/env3/Env3Service.java
@@ -5,9 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.arquitectura.app.excel.FormExcelGetData;
-import com.arquitectura.app.modules.termico.MaterialesRepository;
-import com.arquitectura.app.modules.termico.ResistenciaRepository;
-import com.arquitectura.app.modules.termico.VidriosRepository;
+import com.arquitectura.app.modules.termico.repositories.MaterialesRepository;
+import com.arquitectura.app.modules.termico.repositories.ResistenciaRepository;
+import com.arquitectura.app.modules.termico.repositories.VidriosRepository;
 
 @Service
 public class Env3Service {
