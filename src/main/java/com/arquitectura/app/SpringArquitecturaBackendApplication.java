5111fb9e77f44f3de905cc15c7b72695dbc3136c
unknown <mazher_2014@hotmail.com>
2022-06-16 16:03:02 -0500

    provincias res prod 3

--- src/main/java/com/arquitectura/app/SpringArquitecturaBackendApplication.java
+++ src/main/java/com/arquitectura/app/SpringArquitecturaBackendApplication.java
@@ -29,8 +29,8 @@ public class SpringArquitecturaBackendApplication {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
 	//new comment
-		//corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://precious-melomakarona-380a99.netlify.app"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		//corsConfiguration.setAllowedOrigins(Arrays.asList("https://precious-melomakarona-380a99.netlify.app"));
 
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
