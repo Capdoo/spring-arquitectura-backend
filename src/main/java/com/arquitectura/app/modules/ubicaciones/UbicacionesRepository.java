9d607f3e28c14485c3d05ae5dac9226f49da46f5
unknown <mazher_2014@hotmail.com>
2022-06-16 15:52:37 -0500

    provincias res prod

--- src/main/java/com/arquitectura/app/modules/ubicaciones/UbicacionesRepository.java
+++ src/main/java/com/arquitectura/app/modules/ubicaciones/UbicacionesRepository.java
@@ -1,8 +1,12 @@
 package com.arquitectura.app.modules.ubicaciones;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UbicacionesRepository extends JpaRepository<UbicacionesModel, Integer>{
 	public UbicacionesModel findById(int id);
 	public UbicacionesModel findByProvincia(String provincia);
+	
+	public List<UbicacionesModel> findAll();
 }
--- src/main/java/com/arquitectura/app/modules/ubicaciones/UbicacionesService.java
+++ src/main/java/com/arquitectura/app/modules/ubicaciones/UbicacionesService.java
@@ -1,5 +1,8 @@
 package com.arquitectura.app.modules.ubicaciones;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +10,23 @@ import org.springframework.stereotype.Service;
 public class UbicacionesService {
 	@Autowired
 	UbicacionesRepository ubiRepository;
+	
+	
+	public List<ProvinciasDTO> obtenerTodasProvincias() {
+		List<ProvinciasDTO> listaEnviar = new ArrayList<ProvinciasDTO>();
+	
+		List<UbicacionesModel> listaGeneral = ubiRepository.findAll();
+
+		for(UbicacionesModel p: listaGeneral) {
+			ProvinciasDTO provinciasDTO = new ProvinciasDTO();
+				provinciasDTO.setId(p.getId());
+				provinciasDTO.setNombreProvincia(p.getProvincia());
+			listaEnviar.add(provinciasDTO);
+		}
+		return listaEnviar;
+	}
+	
+	
 	
 	public int obtenerIdPorProvincia(String provincia) {
 		UbicacionesModel ubi = ubiRepository.findByProvincia(provincia);
@@ -16,4 +36,6 @@ public class UbicacionesService {
 	public int obtenerNumeroZona(int id) {
 		return ubiRepository.findById(id).getN_zona();
 	}
+	
+	
 }
